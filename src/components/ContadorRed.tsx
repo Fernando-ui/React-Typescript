19c3d78271d1f54fb4a9d75c9393fb43f2445517
Fernando <fst_96@hotmail.com>
2022-01-25 16:30:17 -0600

    Empezando con useReducer

--- src/components/ContadorRed.tsx
+++ src/components/ContadorRed.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const initialState = {
-    contadorActual:0,
-    contadorAnterior:0,
-}
-const contadorReducer = ( state, action) =>{
+  contador: 0,
+};
 
+type ActionType = {
+  type: 'incrementar'
 };
 
 
+const contadorReducer = (state: typeof initialState, action: ActionType) => {
+  switch (action.type) {
+    case 'incrementar':
+        return{
+            ...state,
+             contador:state.contador + 1,
+        }
+    default:
+        return state;
+  }
+};
+
 export const ContadorRed = () => {
-  return <>
-    <h1>useReff</h1>
-    <h2> Contador: 10</h2>
-  </>;
+  return (                              
+    <>
+      <h1>useReff</h1>
+      <h2> Contador: 10</h2>
+    </>
+  );
 };
